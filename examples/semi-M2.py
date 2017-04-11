Fix bug whereby alpha term averaged over 2 dimension instead of averaging over 1 and summing other.

examples/semi-M2.py
@@ -9,6 +9,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+from numpy import linalg as LA
 import semi_util, importlib
 importlib.reload(semi_util)
 from semi_util import *
@@ -19,21 +20,21 @@ from tensorflow.examples.tutorials.mnist import input_data
 from edward.models import Normal, Bernoulli, Categorical
 
 
-def expand_unlabelled(a, name):
-  """a is of shape (Ml+Mu, None). We expand a to be of shape (M, None) by
-  tiling the part of a corresponding to the unknown labels."""
-  al = a[:Ml]
-  indices = np.repeat(np.arange(Mu), K)
-  au = tf.gather(a[-Mu:], indices)
-  return tf.concat([al, au], axis=0, name=name)
+def repeat_unlabelled(a, name):
+  """a is of shape (Ml+Mu, None). We expand a to be of shape (Ml+Mu*K, None) by
+  repeating the part of a corresponding to the unknown labels."""
+  tf.assert_equal(tf.shape(a)[0], Ml + Mu)
+  return tf.concat([a[:Ml], tf_repeat(a[-Mu:], K)], axis=0, name=name)
 
 
-# a = np.arange(55).reshape((Ml+Mu,1))
+# a = np.arange(Ml+Mu).reshape((Ml+Mu,1))
 # a_tf = tf.constant(a)
-# a_exp_tf = expand_unlabelled(a_tf, 'a')
-# a_exp = a_exp_tf.eval(feed_dict=feed_dict)
-# a[:20]
-# a_exp[:20]
+# a_exp_tf = repeat_unlabelled(a_tf, 'a')
+# sess_test = tf.InteractiveSession()
+# a_exp = sess_test.run(a_exp_tf)
+# a[-20:]
+# a_exp[-20:]
+
 
 
 #
@@ -43,8 +44,8 @@ def expand_unlabelled(a, name):
 
 D = 2    # Number of dimensions of z
 K = 10   # Number of digits to classify
-Ml = 5   # Number of known labels in mini-batch
-Mu = 50  # Number of unknown labels in mini-batch
+Ml = 17  # Number of known labels in mini-batch
+Mu = 19  # Number of unknown labels in mini-batch
 M = Ml + K * Mu  # Mini-batch size (we need K replicates of each unlabelled datum)
 DATA_DIR = "data/mnist"
 IMG_DIR = "img"
@@ -57,15 +58,16 @@ if not os.path.exists(IMG_DIR):
 # The generative model
 #
 # Sample latent variables, z, for both labelled and unlabelled data
-z = Normal(mu=tf.zeros([M, D]), sigma=tf.ones([M, D]), name='z')
+z = Normal(mu=tf.zeros([(Ml+Mu), D]), sigma=tf.ones([(Ml+Mu), D]), name='z')
+z_rep = repeat_unlabelled(z, 'z_exp')
 # Define a y separately for the labelled and unlabelled data and concatenate them
 yu = tf.one_hot(tf.tile(tf.range(K, dtype=tf.int32), (Mu,)), K, name='yu')  # Use one-hot encoding
 yl_ph = tf.placeholder(dtype=yu.dtype, shape=(Ml,K), name='yl')  # The known labels
 y = tf.concat([yl_ph, yu], axis=0, name='y')
 # Define the generative network and the Bernoulli likelihood
-x_logits = generative_network(y, z, K, D, M)
+x_logits = generative_network(y, z_rep, K, D, M)
 xgen = Bernoulli(logits=x_logits, name='xgen')
-hidden_rep = tf.sigmoid(x_logits, name='xp')
+x_probs = tf.sigmoid(x_logits, name='xp')
 
 # Recognition model
 #
@@ -73,17 +75,15 @@ xl_ph = tf.placeholder(dtype=tf.int32, shape=[Ml, 28 * 28], name='xl')
 xu_ph = tf.placeholder(dtype=tf.int32, shape=[Mu, 28 * 28], name='xu')
 xrec = tf.concat([xl_ph, xu_ph], axis=0, name='xrec')
 mu, sigma, y_logits = inference_network(tf.cast(xrec, tf.float32), K, D)
-qz = Normal(
-    mu=expand_unlabelled(mu, 'mu'),
-    sigma=expand_unlabelled(sigma, 'sigma'),
-    name='qz')
+qz = Normal(mu=mu, sigma=sigma, name='qz')
 
 
 # Inference
 #
+xrec_rep = repeat_unlabelled(xrec, 'x_repeated')
 inference = SemiSuperKLqp(
     K=K, Ml=Ml, Mu=Mu, y=y, y_logits=y_logits, alpha=.1*(Ml+Mu),
-    latent_vars={z: qz}, data={xgen: expand_unlabelled(xrec, 'x_expanded')})
+    latent_vars={z: qz}, data={xgen: xrec_rep})
 optimizer = tf.train.AdamOptimizer(0.01, epsilon=1.0)
 debug = False
 n_samples = 1
@@ -106,8 +106,7 @@ avg_losses = []
 n_epoch = 40
 n_iter_per_epoch = 1000
 n_epoch = 40
-n_epoch = 4
-n_iter_per_epoch = 100
+n_iter_per_epoch = 1000
 for _ in range(n_epoch):
   epoch = len(avg_losses)
   total_loss = 0.0
@@ -131,14 +130,15 @@ for _ in range(n_epoch):
   # Check misclassification rate on known labels
   known_labels = np.argmax(yl_ph.eval(feed_dict), axis=1)
   unknown_labels = (yu_train * np.arange(K)).sum(axis=1)
-  pred_labels = np.argmax(inference.y_probs.eval(feed_dict), axis=1)
+  pred_labels = np.argmax(inference.yp.eval(feed_dict), axis=1)
   misclassrate = np.mean(known_labels != pred_labels[:Ml])
-  misclassrate
+  H_qyu = inference.H_qyu.eval(feed_dict)
 
   # Print a lower bound to the average marginal likelihood for an
   # image (and unknown label for unlabelled data).
   avg_loss = total_loss / n_iter_per_epoch / (Ml + Mu)
-  print("Epoch: {:0>3}; avg loss <= {:0.3f}; misclassification rate = {:0.3f}".format(len(avg_losses), avg_loss, misclassrate))
+  print("Epoch: {:0>3}; avg loss <= {:0.3f}; misclassification rate = {:0.3f}; avg H_qyu = {:0.3f}".format(
+    len(avg_losses), avg_loss, misclassrate, H_qyu.mean()))
   avg_losses += [avg_loss]
 
   # Interpolate between 2 random points in latent z-space and plot one digit for each
@@ -146,21 +146,32 @@ for _ in range(n_epoch):
   zsampled = np.zeros((M, D))
   numstyles = int(M / K)
   numimages = numstyles * K
-  # 2 random points in z-space
-  z1, z2 = np.random.normal(loc=0, scale=2., size=(2,D))
-  # Interpolate between them
-  zstyles = z1 + np.linspace(0, 1, num=numstyles).reshape((numstyles ,1)) * (z2 - z1).reshape((1, D))
+  # 1 random point in z-space
+  z1 = np.random.normal(loc=0, scale=1., size=D)
+  z1 = 2 * z1 / LA.norm(z1, 2)  # Make z1 of length 2
+  # Interpolate between z1 and -z1
+  zstyles = z1 - np.linspace(0, 1, num=numstyles).reshape((numstyles ,1)) * 2 * z1.reshape((1, D))
   zsampled[:numimages] = np.tile(zstyles, (K, 1))
   ysampled[np.arange(numimages), np.repeat(np.arange(K), numstyles)] = 1  # One hot encoding
-  imgs = sess.run(hidden_rep, feed_dict={z: zsampled, y: ysampled})[:numimages]
+  imgs = sess.run(x_probs, feed_dict={z_rep: zsampled, y: ysampled})[:numimages]
   create_image_array(imgs, rows=K).save(
       os.path.join(IMG_DIR, 'semi-M2-{:0>3}.png'.format(epoch)))
 
-  # Examine the entropy of the y logits
-  H_qy = inference.H_qy.eval(feed_dict)
+  # Plot the entropy of the y logits
   plt.figure()
-  sns.kdeplot(H_qy)
-  plt.savefig(os.path.join(IMG_DIR, 'H_qy-{:0>3}.png'.format(epoch)))
+  sns.kdeplot(H_qyu)
+  plt.savefig(os.path.join(IMG_DIR, 'H_qyu-{:0>3}.png'.format(epoch)))
   plt.close()
 
   # raise ValueError()
+
+# Plots of losses over epochs
+#
+plt.figure()
+plt.plot(avg_losses)
+plt.savefig('img/avg_losses.png')
+plt.close()
+plt.figure()
+plt.plot(avg_losses[10:])  # Ignore first few losses to zoom in on convergence
+plt.savefig('img/avg_losses_last.png')
+plt.close()

examples/semi_util.py
@@ -13,6 +13,20 @@ from edward.models import RandomVariable
 from edward.util import copy
 
 
+def tf_repeat(a, times):
+  """Similar to np.repeat with axis=0."""
+  indices = np.repeat(np.arange(a.shape[0].value), times)
+  return tf.gather(a, indices)
+
+
+# a = np.arange(3)
+# a_tf = tf.constant(a)
+# a_rep_tf = tf_repeat(a_tf, 2)
+# sess_test = tf.Session()
+# a_rep = sess_test.run(a_rep_tf)
+# a_rep
+
+
 def create_image_array(imgs, rows=None, cols=None):
   N = len(imgs)
   if rows is None:
@@ -86,6 +100,10 @@ class SemiSuperKLqp(ed.KLqp):
     self.y = y
     self.y_logits = y_logits
     self.alpha = alpha
+    # Indices into unlabelled data structures
+    self.m = self.Ml + np.repeat(np.arange(self.Mu), self.K)
+    self.k = np.tile(np.arange(self.K), self.Mu)
+    self.mk = self.Ml + np.arange(self.Mu * self.K)
 
   def build_loss_and_gradients(self, var_list):
     "Adapted from edward.inferences.klqp.build_reparam_kl_loss_and_gradients"
@@ -120,42 +138,41 @@ class SemiSuperKLqp(ed.KLqp):
           log_probs = self.scale.get(x, 1.0) * x_copy.log_prob(dict_swap[x])
           self.p_log_lik[s] += tf.reduce_mean(log_probs, axis=1)
 
-    # Stack the list of log likeihoods for each iteration into one tensor
+    # Stack the list of log likelihoods for each iteration into one tensor
     # and average across samples
     self.p_log_lik = tf.reduce_mean(
         tf.stack(self.p_log_lik), axis=0, name='p_log_lik')
 
     # Calculate the KL divergences between q(z|x) and p(z)
-    self.kl = tf.stack([
-        self.kl_scaling.get(z, 1.0) * ed.inferences.klqp._calc_KL(qz, z)
-        for z, qz in six.iteritems(self.latent_vars)])
+    self.kl = tf.concat(
+        [ self.kl_scaling.get(z, 1.0) * ed.inferences.klqp._calc_KL(qz, z)
+          for z, qz in six.iteritems(self.latent_vars)],
+        axis=0)
     # sum over latent z dimensions and qz distributions
-    self.kl = tf.reduce_sum(self.kl, axis=(0, 2), name='kl')
+    self.kl = tf.reduce_sum(self.kl, axis=1, name='kl')
     # print(self.kl)
 
-    # Calculate the standard variational bound assuming we know the labels
-    self.L = -(self.p_log_lik - self.kl)
-    self.L = tf.identity(self.L, name='L')
-    # print(self.L)
+    # Calculate L for the labelled data
+    self.Ll = self.kl[:self.Ml] - self.p_log_lik[:self.Ml]
 
     # Entropy of q(z|x) for unlabelled data
-    self.y_probs = tf.nn.softmax(self.y_logits, name='y_probs')
-    # print(self.y_probs)
-    self.H_qy = tf_entropy(self.y_probs[self.Ml:])
-    self.H_qy = tf.identity(self.H_qy, name='H_qy')
-    # print(self.H_qy)
+    self.yp = tf.nn.softmax(self.y_logits, name='yp')
+    # print(self.yp)
+    self.H_qyu = tf_entropy(self.yp[self.Ml:])
+    self.H_qyu = tf.identity(self.H_qyu, name='H_qyu')
+    # print(self.H_qyu)
 
     # Contribution to loss from unlabelled data
-    self.m = np.repeat(np.arange(self.Mu), self.K)
-    self.k = np.tile(np.arange(self.K), self.Mu)
-    self.U = tf.reduce_sum([
-        self.y_probs[self.Ml + m, k] * self.L[self.Ml + i]
-        for i, (m, k) in enumerate(zip(self.m, self.k))
-    ]) - tf.reduce_sum(self.H_qy)
+    self.klu = tf.reshape(tf_repeat(self.kl[self.Ml:], self.K), (self.Mu, self.K))
+    self.llu = tf.reshape(self.p_log_lik[self.Ml:], (self.Mu, self.K))
+    self.Lu = self.klu - self.llu
+    self.ypu = self.yp[self.Ml:]
+    self.U = tf.reduce_sum(tf.reduce_sum(self.ypu * self.Lu, axis=1) - self.H_qyu)
 
     # Calculate J and Jalpha
-    self.J = tf.reduce_sum(self.L[:self.Ml]) + self.U
-    alpha_term = - self.alpha * tf.reduce_mean(self.y[:self.Ml] * tf.log(self.y_probs[:self.Ml]))
+    self.J = tf.reduce_sum(self.Ll) + self.U
+    # alpha term has a sum over K and average over Ml
+    alpha_term = - self.alpha * tf.reduce_mean(tf.reduce_sum(self.y[:self.Ml] * tf.log(self.yp[:self.Ml]), axis=1))
     self.Jalpha = self.J + alpha_term
 
     # Calculate gradients